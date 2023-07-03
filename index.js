c46de5ebe6ce0c669cfbacdc85b04356c5a1bc15
[SW5] 이시훈 <elice.sihun@gmail.com>
2023-07-04 07:46:18 +0900

    feat: 로그인 체크 함수 추가(JWT 토큰 x)

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-// const { User } = require("./models");
 const { user } = require("./services/user");
 // const postsRouter = require("./routes/posts");
 
@@ -13,11 +12,20 @@ mongoose.connection.on("connected", () => {
   console.log("Successfully connected to MongoDB");
 });
 
+// const middleware = async (req, res) => {
+//   res.send("OK");
+//   const check = await user.register({
+//     name: "test2",
+//     email: "test2@gmail.com",
+//     pw: "1234",
+//   });
+//   console.log(check);
+// };
+
 const middleware = async (req, res) => {
   res.send("OK");
-  const check = await user.add({
-    name: "test",
-    email: "이메일중복테스트",
+  const check = await user.login({
+    email: "test@gmail.com",
     pw: "1234",
   });
   console.log(check);
@@ -25,11 +33,11 @@ const middleware = async (req, res) => {
 
 app.get("/", middleware);
 
-app.get("/:name", async (req, res) => {
-  const name = req.params.name;
-  const user = await User.find({ name: name });
-  res.send(user);
-});
+// app.get("/:name", async (req, res) => {
+//   const name = req.params.name;
+//   const user = await User.find({ name: name });
+//   res.send(user);
+// });
 
 // app.use("/posts", postsRouter);
 
--- services/user.js
+++ services/user.js
@@ -5,17 +5,36 @@ class UserService {
     this.userModel = User;
   }
 
-  async add(userInfo) {
+  //회원가입
+  async register(userInfo) {
     const { name, email, pw } = userInfo;
 
+    //이메일 중복 체크
     const check = await this.userModel.find({ email: email });
     if (check.length !== 0) {
       throw new Error("중복되는 이메일 주소 입니다.");
     }
-    // console.log(check);
 
     const newUser = await this.userModel.create({ name, email, pw });
     return newUser;
+  }
+
+  //로그인
+  async login(userInfo) {
+    const { email, pw } = userInfo;
+
+    //회원정보에 존재하는 이메일인지 체크
+    const check = await this.userModel.find({ email: email });
+    if (check.length === 0) {
+      throw new Error("존재하지 않는 이메일 주소 입니다.");
+    }
+
+    //비밀 번호 일치 여부 체크
+    if (check[0].pw !== pw) {
+      throw new Error("비밀번호가 일치하지 않습니다.");
+    }
+
+    //JWT 토큰 관련 추가 필요
   }
 }
 
