c85127a01739b03aaab2e2e560384b05862b4347
[SW5] 이시훈 <elice.sihun@gmail.com>
2023-07-04 13:12:44 +0900

    feat: 로그인, 비밀번호 수정, 회원 탈퇴 미들웨어 추가

--- controller/middlewares/user.js
+++ controller/middlewares/user.js
@@ -1,14 +1,41 @@
 const { user } = require("../../services/user");
 
 const register = async (req, res) => {
-  const check = await user.register({
+  const user = await user.register({
     name: req.body.name,
     email: req.body.email,
     pw: req.body.pw,
   });
-  res.send(check);
-  console.log(check);
-  //console.log(req.body);
+  res.send(user);
+  console.log(user);
+};
+
+const login = async (req, res) => {
+  const token = await user.login({
+    email: req.body.email,
+    pw: req.body.pw,
+  });
+  res.cookie("token", token);
+  console.log(token);
+};
+
+const updatePw = async (req, res) => {
+  const update = await user.login({
+    email: req.body.email,
+    pw: req.body.pw,
+    newPw: req.body.newPw,
+  });
+  res.send(update);
+  console.log(update);
+};
+
+const deleteAccount = async (req, res) => {
+  const result = await user.login({
+    email: req.body.email,
+    pw: req.body.pw,
+  });
+  res.send(result);
+  console.log(result);
 };
 
 module.exports = { register };
