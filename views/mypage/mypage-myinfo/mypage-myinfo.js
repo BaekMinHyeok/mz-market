bd26ee9ef4d642bed052d157cad997328cf64d70
[SW5] 유종훈 <soojhyoung@gmail.com>
2023-07-05 18:13:00 +0900

    fix: 수정버튼 클릭시 console.log를 alert로 변경

--- views/mypage/mypage-myinfo/mypage-myinfo.js
+++ views/mypage/mypage-myinfo/mypage-myinfo.js
@@ -15,7 +15,7 @@ deleteAccountButton.addEventListener('click', deleteAccount);
 async function editName() {
   const newName = nameInput.value;
   try {
-    const response = await fetch('/update-name', {
+    const response = await fetch('/edit-name', {
       method: 'PUT',
       headers: {
         'Authorization': `Bearer ${localStorage.getItem('jwtToken')}`, //header에 jwt토큰 추가
@@ -24,19 +24,19 @@ async function editName() {
       body: JSON.stringify({ name: newName })
     });
     if (response.ok) {
-      console.log('이름을 변경하였습니다.');
+      alert('이름을 변경하였습니다.');
     } else {
-      console.error('이름 변경에 실패했습니다.');
+      alert('이름 변경에 실패했습니다.');
     }
   } catch (error) {
-    console.error('이름 변경에 실패했습니다.:', error);
+    alert('이름 변경에 실패했습니다.:', error);
   }
 }
 
 async function editEmail() {
   const newEmail = emailInput.value;
   try {
-    const response = await fetch('/update-email', {
+    const response = await fetch('/edit-email', {
       method: 'PUT',
       headers: {
         'Authorization': `Bearer ${localStorage.getItem('jwtToken')}`,//header에 jwt토큰 추가
@@ -45,12 +45,12 @@ async function editEmail() {
       body: JSON.stringify({ email: newEmail })
     });
     if (response.ok) {
-      console.log('이메일이 변경되었습니다.');
+      alert('이메일이 변경되었습니다.');
     } else {
-      console.error('이메일변경에 실패하였습니다.');
+      alert('이메일변경에 실패하였습니다.');
     }
   } catch (error) {
-    console.error('이메일변경에 실패했습니다:', error);
+    alert('이메일변경에 실패했습니다:', error);
   }
 }
 
@@ -59,12 +59,12 @@ async function editPassword() {
   const newPasswordCheck = passwordCheckInput.value;
 
   if (newPassword !== newPasswordCheck) {
-    console.error('비밀번호가 일치하지 않습니다!');
+    alert('비밀번호가 일치하지 않습니다!');
     return;
   }
 
   try {
-    const response = await fetch('/update-password', {
+    const response = await fetch('/edit-password', {
       method: 'PUT',
       headers: {
         'Authorization': `Bearer ${localStorage.getItem('jwtToken')}`, //header에 jwt토큰 추가
@@ -73,12 +73,12 @@ async function editPassword() {
       body: JSON.stringify({ password: newPassword })
     });
     if (response.ok) {
-      console.log('비밀번호를 변경하였습니다.');
+      alert('비밀번호를 변경하였습니다.');
     } else {
-      console.error('비밀번호 변경에 실패하였습니다!');
+      alert('비밀번호 변경에 실패하였습니다!');
     }
   } catch (error) {
-    console.error('비밀번호 변경에 실패하였습니다:', error);
+    alert('비밀번호 변경에 실패하였습니다:', error);
   }
 }
 
@@ -92,11 +92,11 @@ async function deleteAccount() {
       }
     });
     if (response.ok) {
-      console.log('회원탈퇴 되었습니다.');
+      alert('회원탈퇴 되었습니다.');
     } else {
-      console.error('회원탈퇴에 실패하였습니다!');
+      alert('회원탈퇴에 실패하였습니다!');
     }
   } catch (error) {
-    console.error('회원탈퇴에 실패하였습니다:', error);
+    alert('회원탈퇴에 실패하였습니다:', error);
   }
 }
