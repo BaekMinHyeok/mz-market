5793b152a81175418e1e30a6a84ae18a8394831c
[SW5] 이시훈 <elice.sihun@gmail.com>
2023-07-06 01:47:43 +0000

    Update api.js

--- routers/api.js
+++ routers/api.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-<<<<<<< routers/api.js
 const {
   register,
   login,
@@ -21,22 +20,5 @@ router.get("/getAllProduct", getAllProduct) //모든 상품 목록 가져오기
 router.get("/getProductByName", getProductByName) //특정 상품의 상세 정보 가져오기
 router.put("/updateProduct", updateProduct) //상품 업데이트
 router.delete("/deleteProduct", deleteProduct) //상품 삭제
-
-
-=======
-const {
-  register,
-  login,
-  updateUser,
-  getUser,
-  deleteUser,
-} = require("../controller/user");
->>>>>>> routers/api.js
-
-router.post("/register", register); //회원 가입
-router.post("/login", login); //로그인
-router.post("/update_user", updateUser); //유저정보 수정
-router.post("/get_user", getUser); //유저정보 조회
-router.post("/delete_user", deleteUser); //회원 탈퇴
 
 module.exports = router;
