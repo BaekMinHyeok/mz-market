5975843ec7c8c46419ac95c0603c901f69492e3b
[SW5] 이시훈 <elice.sihun@gmail.com>
2023-07-06 14:15:10 +0900

    feat: 이미지 업로드 모듈화

--- controller/product.js
+++ controller/product.js
@@ -1,19 +1,21 @@
-const { product } = require("../services/user") ;
+const { product } = require("../services/user");
 // const upload = multer({ dest: 'uploads/' });
 const upload = require("../middlewares/multerconfig");
 
 //상품등록
 const registerProduct = async (req, res) => {
   try {
+    const image = uploadImg(req, res);
     const newProduct = await product.registerProduct({
       name: req.body.name,
       description: req.body.description,
       price: req.body.price,
       category: req.body.category,
       gender: req.body.gender,
-    })
+      images: image.map((img) => img.filename),
+    });
     res.send(newProduct);
-  } catch(error) {
+  } catch (error) {
     //res.status(500).send("상품 등록에 실패했습니다.");
   }
 };
@@ -21,11 +23,13 @@ const registerProduct = async (req, res) => {
 // 상품 업데이트
 const updateProduct = async (req, res) => {
   try {
-    const productId = req.params.productId
-    const updatedInfo = req.body
+    const image = uploadImg(req, res);
+    const productId = req.params.productId;
+    const updatedInfo = req.body;
+    updatedInfo.images = image.map((img) => img.filename);
     const updatedProduct = await product.updateProduct(productId, updatedInfo);
 
-    res.send(updatedProduct)
+    res.send(updatedProduct);
   } catch (err) {
     // res.status(500).json({
     //   error : "제품 업데이트에 실패했습니다.",
@@ -42,38 +46,14 @@ const uploadImg = async (req, res) => {
         // return res.status(500).json({ error: "이미지 업로드에 실패했습니다." });
       }
 
-      const images = req.files;
-      const isRegister = req.body.isRegister
-      
-
-      try {
-        let newProduct;
-        const productData = {
-          name: req.body.name,
-          description: req.body.description,
-          price: req.body.price,
-          category: req.body.category,
-          images: images.map((image) => image.filename),
-          gender: req.body.gender,
-        }
-        
-        if(isRegister){
-        newProduct = await product.registerProduct(productData);
-      } else {
-        newProduct = await product.updateProduct(productData)
-      }
-
-        return res.json(newProduct);
-      } catch (error) {
-        // return res.status(500).json({ error: "상품 등록에 실패했습니다." });
-      }
+      return req.files;
     });
   } catch (error) {
     // return res.status(500).json({ error: "이미지 업로드에 실패했습니다." });
   }
 };
 
-// 모든 상품 목록 
+// 모든 상품 목록
 const getAllProduct = async (req, res) => {
   try {
     const allProduct = await product.getAllProduct();
@@ -82,7 +62,7 @@ const getAllProduct = async (req, res) => {
     // res.status(500).json({
     //  error: "상품 목록 조회에 실패했습니다.",
     // });
-   }
+  }
 };
 
 // 특정 상품 목록
@@ -93,19 +73,16 @@ const getProductByName = async (req, res) => {
     });
 
     res.json(productByName);
-  } catch (err) {
-
-  }
+  } catch (err) {}
 };
-
 
 // 상품 삭제
 
 const deleteProduct = async (req, res) => {
-  try{
-    const productId = req.params.productId;  
+  try {
+    const productId = req.params.productId;
     const deletedProduct = await product.deleteProduct(productId);
-    
+
     res.send(deletedProduct);
   } catch (err) {
     // res.status(500).json({
@@ -114,7 +91,11 @@ const deleteProduct = async (req, res) => {
   }
 };
 
-
-
-
-module.exports = { registerProduct, uploadImg, getAllProduct, getProductByName,updateProduct, deleteProduct} 
+module.exports = {
+  registerProduct,
+  uploadImg,
+  getAllProduct,
+  getProductByName,
+  updateProduct,
+  deleteProduct,
+};
