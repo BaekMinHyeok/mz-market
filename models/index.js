260fbaa9a3b803259ec95c6cd9939e5f691b8c3d
[SW5] 이시훈 <elice.sihun@gmail.com>
2023-07-07 16:14:45 +0900

    feat: category

--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const userSchema = require("./schemas/user");
 const productSchema = require("./schemas/product");
+const categorySchema = require("./schemas/category");
 
 exports.User = mongoose.model("User", userSchema);
 exports.Product = mongoose.model("Product", productSchema);
+exports.Category = mongoose.model("Category", categorySchema);
