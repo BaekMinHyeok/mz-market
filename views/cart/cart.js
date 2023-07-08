842a02a7bd03ff0c793204c7a355e1267c988077
[SW5] 예은선 <ysvln0912@gmail.com>
2023-07-09 00:41:17 +0900

    feat : 장바구니 페이지 상품수, 총 금액 표시

--- views/cart/cart.js
+++ views/cart/cart.js
@@ -62,7 +62,7 @@ if (cartData) {
         localStorage.setItem("cart", JSON.stringify(cartItems));
 
         console.log("11111" + cartData);
-
+        updateTotalQuantityAndPrice();
         // 수량 증가
       } else if (event.target.classList.contains("product_add")) {
         quantity++;
@@ -78,7 +78,7 @@ if (cartData) {
 
         // 업데이트된 장바구니 데이터를 로컬 스토리지에 저장
         localStorage.setItem("cart", JSON.stringify(cartItems));
-
+        updateTotalQuantityAndPrice();
         console.log("22222" + cartData);
       }
     });
@@ -123,6 +123,7 @@ removeAllButton.addEventListener("click", function () {
 // 총 상품 수량 계산
 function calculateTotalQuantity() {
   const productNumElements = document.querySelectorAll(".product_num");
+  console.log(productNumElements.textContent);
   let totalQuantity = 0;
 
   productNumElements.forEach((element) => {
@@ -136,12 +137,18 @@ function calculateTotalQuantity() {
 // 총 상품 합계 계산
 function calculateTotalPrice() {
   const productPriceElements = document.querySelectorAll(".product_price");
+  const productNumElements = document.querySelectorAll(".product_num");
   let totalPrice = 0;
 
-  productPriceElements.forEach((element) => {
-    const price = parseInt(element.textContent);
-    totalPrice += price;
+  productPriceElements.forEach((element, index) => {
+    const priceText = element.textContent;
+    const price = parseInt(priceText);
+    const quantity = parseInt(productNumElements[index].textContent);
+    const itemTotalPrice = price * quantity;
+    totalPrice += itemTotalPrice;
   });
+
+  console.log("총 가격: " + totalPrice);
 
   return totalPrice;
 }
@@ -150,20 +157,32 @@ function calculateTotalPrice() {
 function updateTotalQuantityAndPrice() {
   const countElement = document.querySelector(".count");
   const priceElement = document.querySelector(".price");
+  const deliverElement = document.querySelector(".deliver");
+  const totalPriceElement = document.querySelector(".nav_total_price");
 
   const totalQuantity = calculateTotalQuantity();
   const totalPrice = calculateTotalPrice();
+  const deliveryFee = 3000; // 배송비 설정
+
+  const finalPrice = totalPrice + deliveryFee;
 
   countElement.textContent = totalQuantity.toString();
   priceElement.textContent = totalPrice.toString();
+  deliverElement.textContent = deliveryFee.toString();
+  totalPriceElement.textContent = finalPrice.toString();
 }
 
 // 수량 수정 시 총 상품 수량 및 합계 업데이트
-document.addEventListener("click", function (event) {
-  if (
-    event.target.classList.contains("product_remove") ||
-    event.target.classList.contains("product_add")
-  ) {
-    updateTotalQuantityAndPrice();
-  }
+// document.addEventListener("click", function (event) {
+//   if (
+//     event.target.classList.contains("product_remove") ||
+//     event.target.classList.contains("product_add") ||
+//     event.target.classList.contains("order_remove_button")
+//   ) {
+//     updateTotalQuantityAndPrice(); // 총 상품 수량 및 금액 업데이트
+//   }
+// });
+
+window.addEventListener("DOMContentLoaded", function () {
+  updateTotalQuantityAndPrice(); // 초기화 함수 호출
 });
