5dce265b37bca9a48694672ef9df1acbc02f8897
[SW5] 백민혁 <minhyeokbaek100@gmail.com>
2023-07-10 16:58:36 +0900

    category

--- controller/category.js
+++ controller/category.js
@@ -37,8 +37,10 @@ const updateCategory = async (req, res) => {
 //카테고리 삭제
 const deleteCategory = async (req, res) => {
   try {
-    const { name } = req.body;
-    await category.delete({ name });
+    const categoryId = req.params.categoryId
+    await category.deleteCategory(categoryId)
+    // const { name } = req.body;
+    // await category.delete({ name });
     res.json({
       success: true,
       message: "카테고리를 삭제했습니다.",
