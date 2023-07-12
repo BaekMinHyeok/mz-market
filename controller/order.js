17462bfc1e6a0e5abfeb4edf55880001da27a6a8
[SW5] 백민혁 <minhyeokbaek100@gmail.com>
2023-07-12 09:59:23 +0900

    refactor order 수정

--- controller/order.js
+++ controller/order.js
@@ -4,9 +4,8 @@ const { product } = require("../services/product");
 // 주문 등록
 const registerOrder = async (req, res) => {
   try {
-    const { name, phoneNumber, address, address2, comments, productId} = req.body;
-    console.log(product);
-    const productInfo = await product.getProductById(productId);
+    const { name, phoneNumber, address, address2, comments, objectId, price, quantity} = req.body;
+    const productInfo = await product.getProductByObjectId(objectId);
     if (!productInfo){
       throw "상품을 찾을 수 없습니다.";
     }
@@ -17,44 +16,25 @@ const registerOrder = async (req, res) => {
       address2,
       comments,
       status: "ready",
-      productId,
+      product: objectId,
+      price,
+      quantity
     });
     res.json({
       success: true,
       message: "주문 등록에 성공했습니다.",
+      orders: productInfo
     });
   } catch (error) {
     res.json({
       success: false,
       message: error,
+      orders: undefined
     });
     console.log(error);
   }
 };
 
-
-// const registerOrder = async (req, res) => {
-//   try {
-//     const { name, phoneNumber, address, address2, comments } = req.body;
-//     await order.register({
-//       name,
-//       phoneNumber,
-//       address,
-//       address2,
-//       comments,
-//     });
-//     res.json({
-//       success: true,
-//       message: "주문 등록에 성공했습니다.",
-//     });
-//   } catch (error) {
-//     res.json({
-//       success: false,
-//       message: error,
-//     });
-//     console.log(error);
-//   }
-// };
 
 // 주문 정보 수정
 const updateOrder = async (req, res) => {
@@ -74,6 +54,7 @@ const updateOrder = async (req, res) => {
     });
   }
 };
+
 
 // 주문 정보 조회
 const getAllOrders = async (req, res) => {
