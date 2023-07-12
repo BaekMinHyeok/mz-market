6b6fbc35a477b69fb230c1e8438cd7b147e4e566
[SW5] 김석민 <gudwp815@gmail.com>
2023-07-13 00:38:44 +0900

    화이팅~~~~git add .

--- public/header/header.js
+++ public/header/header.js
@@ -1,20 +1,33 @@
-try {
   fetch("/header/header.html")
     .then((res) => res.text())
     .then((html) => {
-      document.body.insertAdjacentHTML("afterbegin", html);
+      document.body.innerHTML = html + document.body.innerHTML;
       const logoBtn = document.querySelector("#logoBtn");
+      const menBtn = document.querySelector("#menBtn");
+      const womenBtn = document.querySelector("#womenBtn");
+      const loginBtn = document.querySelector("#loginBtn");
+      const cartBtn = document.querySelector("#cartBtn");
+
       logoBtn.addEventListener("click", function () {
         window.location.href = "/";
       });
-      // JavaScript 파일 로드
+      menBtn.addEventListener("click", function () {
+        window.location.href = `/category/men`;
+      });
+      womenBtn.addEventListener("click", function () {
+        window.location.href = "/category/women";
+      });
+      loginBtn.addEventListener("click", function () {
+        window.location.href = "/user/sign_in";
+      });
+      cartBtn.addEventListener("click", function () {
+        window.location.href = "/cart";
+      });
+
     })
     .catch((error) => {
       console.error("Error fetching header content:", error);
     });
-} catch (error) {
-  console.error("An error occurred:", error);
-}
 
 
 
