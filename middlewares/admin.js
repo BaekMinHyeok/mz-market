da5a7416bd0c999df296d451f615f12c301e9083
[SW5] 이시훈 <elice.sihun@gmail.com>
2023-07-13 08:31:17 +0000

    Update admin.js

--- middlewares/admin.js
+++ middlewares/admin.js
@@ -25,6 +25,35 @@ const adminAuth = async (req, res, next) => {
   }
 };
 
+//어드민 체크
+const adminCheck = async (req, res) => {
+  try {
+    const decoded = jwt.verify(
+      req.headers.authorization.split(" ")[1],
+      process.env.SECRET
+    );
+    if (decoded.admin) {
+      return res.json({
+        success: true,
+      });
+    } else {
+      return res.josn({
+        success: false,
+        message: "관리자만 접근 가능한 페이지입니다!",
+      });
+    }
+  } catch (error) {
+    // 토큰의 키가 일치하지 않는 경우
+    if (error.name === "JsonWebTokenError") {
+      return res.json({
+        success: false,
+        message: "유효하지 않은 토큰입니다!!",
+      });
+    }
+  }
+};
+
 module.exports = {
   adminAuth,
+  adminCheck
 };
