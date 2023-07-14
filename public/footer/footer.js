fa90f4200032e6d237d82ed19aeb4db1fc6d7d80
[SW5] 유종훈 <soojhyoung@gmail.com>
2023-07-14 15:21:39 +0900

    fix: footer copyright

--- public/footer/footer.js
+++ public/footer/footer.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   const footer = document.createElement('footer');
-  footer.innerHTML = '<div class="container"><p>&copy; mz-market. All rights reserved.</p></div>';
+  footer.innerHTML = '<div class="container"><p>Copyright 2023. mz-market.</p> <p>백없는 팀.</p> <p>All rights reserved.</p></div>';
   footer.classList.add('footer-style');
   document.body.appendChild(footer);
 });
